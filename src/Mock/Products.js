0167be1e5c9d082d4d25b8efd576a545e9b2ca54
Mendieta Sergio <mendieta.sergio.1995@gmail.com>
2022-05-12 00:06:26 -0300

    Añade mock con productos

---
+++ src/Mock/Products.js
@@ -0,0 +1,50 @@
+export const products = [
+    { 
+        id: 1,
+        categorie: "Pollo",
+        img:"HM_pollo.jpg",
+        description:"Lorem ipsum dolor sit amet, consectetur adipisicing"
+    },
+    { 
+        id: 2,
+        categorie: "Pollo",
+        img:"HM_pollo2.jpg",
+        description:"Lorem ipsum dolor sit amet, consectetur adipisicing"
+    },
+    { 
+        id: 3,
+        categorie: "Pollo",
+        img:"HM_pollo3.jpg",
+        description:"Lorem ipsum dolor sit amet, consectetur adipisicing"
+    },
+    { 
+        id: 4,
+        categorie: "Carne",
+        img:"HM_carne.jpg",
+        description:"Lorem ipsum dolor sit amet, consectetur adipisicing"
+    },
+    { 
+        id: 5,
+        categorie: "Carne",
+        img:"HM_carne2.jpg",
+        description:"Lorem ipsum dolor sit amet, consectetur adipisicing"
+    },
+    { 
+        id: 6,
+        categorie: "Carne",
+        img:"HM_carne3.jpg",
+        description:"Lorem ipsum dolor sit amet, consectetur adipisicing"
+    },
+    { 
+        id: 7,
+        categorie: "Vegetariana",
+        img:"HM_vegan.jpg",
+        description:"Lorem ipsum dolor sit amet, consectetur adipisicing"
+    },
+    { 
+        id: 8,
+        categorie: "Vegetariana",
+        img:"HM_vegan2.jpg",
+        description:"Lorem ipsum dolor sit amet, consectetur adipisicing"
+    },
+];
