6b6c9a5f062c71eee44d18f5b9564f781a6500bb
Mendieta Sergio <mendieta.sergio.1995@gmail.com>
2022-05-12 01:04:05 -0300

    Añade componentes en app.jsx para renderizarlos

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import './App.css'
+import { Carousel } from './components/Carousel/Carousel'
 import { Header } from './components/Header/Header'
+import { Services } from './components/Services/Services'
+import { Location } from './components/Location/Location'
+import { Footer } from './components/Footer/Footer'
 
 function App() {
 
   return (
-    <div className="App">
+    <div className="">
       <Header />
+      <div className="container">
+        <Carousel/>
+        <Services />
+        <Location />
+      </div>
+      <Footer />
     </div>
   )
 }
