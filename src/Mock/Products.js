4af33b849f339375b38d36070ace0c119575ef13
Mendieta Sergio <mendieta.sergio.1995@gmail.com>
2022-05-14 11:22:59 -0300

    Añade url a img

--- src/Mock/Products.js
+++ src/Mock/Products.js
@@ -2,49 +2,49 @@ export const products = [
     { 
         id: 1,
         categorie: "Pollo",
-        img:"HM_pollo.jpg",
+        img:"https://res.cloudinary.com/freelance01/image/upload/v1652537821/Cangre-Burger/HM_pollo3_p4ivn6.jpg",
         description:"Lorem ipsum dolor sit amet, consectetur adipisicing"
     },
     { 
         id: 2,
         categorie: "Pollo",
-        img:"HM_pollo2.jpg",
+        img:"https://res.cloudinary.com/freelance01/image/upload/v1652537821/Cangre-Burger/HM_pollo_zaa0d2.jpg",
         description:"Lorem ipsum dolor sit amet, consectetur adipisicing"
     },
     { 
         id: 3,
         categorie: "Pollo",
-        img:"HM_pollo3.jpg",
+        img:"https://res.cloudinary.com/freelance01/image/upload/v1652537820/Cangre-Burger/HM_pollo2_cj1ecs.jpg",
         description:"Lorem ipsum dolor sit amet, consectetur adipisicing"
     },
     { 
         id: 4,
         categorie: "Carne",
-        img:"HM_carne.jpg",
+        img:"https://res.cloudinary.com/freelance01/image/upload/v1652537821/Cangre-Burger/HM_carne2_srypbx.jpg",
         description:"Lorem ipsum dolor sit amet, consectetur adipisicing"
     },
     { 
         id: 5,
         categorie: "Carne",
-        img:"HM_carne2.jpg",
+        img:"https://res.cloudinary.com/freelance01/image/upload/v1652537822/Cangre-Burger/HM_carne_d5jvnk.jpg",
         description:"Lorem ipsum dolor sit amet, consectetur adipisicing"
     },
     { 
         id: 6,
         categorie: "Carne",
-        img:"HM_carne3.jpg",
+        img:"https://res.cloudinary.com/freelance01/image/upload/v1652537822/Cangre-Burger/HM_carne3_brxqds.jpg",
         description:"Lorem ipsum dolor sit amet, consectetur adipisicing"
     },
     { 
         id: 7,
         categorie: "Vegetariana",
-        img:"HM_vegan.jpg",
+        img:"https://res.cloudinary.com/freelance01/image/upload/v1652537821/Cangre-Burger/HM_vegan2_zrmjq0.jpg",
         description:"Lorem ipsum dolor sit amet, consectetur adipisicing"
     },
     { 
         id: 8,
         categorie: "Vegetariana",
-        img:"HM_vegan2.jpg",
+        img:"https://res.cloudinary.com/freelance01/image/upload/v1652537821/Cangre-Burger/HM_vegan_frxlqq.jpg",
         description:"Lorem ipsum dolor sit amet, consectetur adipisicing"
     },
 ];
